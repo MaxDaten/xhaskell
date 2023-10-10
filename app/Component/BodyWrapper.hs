26b48e308764ec4c95b76c1e8693390dff8c6648
Jan-Philip Loos <maxdaten@gmail.com>
2023-10-10 04:55:49 +0200

    feat: Small Header adjustments

--- app/Component/BodyWrapper.hs
+++ app/Component/BodyWrapper.hs
@@ -8,7 +8,8 @@ bodyWrapper :: (Monad m) => HtmlT m () -> HtmlT m ()
 bodyWrapper content =
   body_ [class_ "bg-gradient-to-br from-gray-900/50 via-blue-900/50 to-blue-950/50 w-full h-screen"] $ do
     header_ [class_ "text-center py-16 text-white"] $
-      h1_ [class_ "text-4xl text-purple-500"] "Ask Anyting!"
+      h1_ [class_ "text-4xl font-black font-serif text-purple-500"] $
+        do span_ [class_ "text-purple-200 underline"] "Ask" <> span_ " Anyting!"
     main_ [class_ "flex flex-col justify-center my-10"] $ do
       div_ [class_ "flex justify-center"] $ do
         div_ [class_ "w-1/2 border rounded-3xl bg-violet-500 bg-gradient-to-br from-purple-600 via-violet-600 to-indigo-600 border-gray-400 px-16 py-8 bg-gray-300 shadow-indigo-500/50 shadow-lg ring-1 ring-inset ring-white/20"] $ do
