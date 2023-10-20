88e5c747a8c6ec30c0a39df8bec29c9e0e6c5ac7
Jan-Philip Loos <maxdaten@gmail.com>
2023-10-20 05:27:42 +0200

    chore: terraform fmt

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "gcs" {
-        # Managed by gitops
-        bucket  = "ai-playground-c437-terraform-state"
-        prefix  = "xhaskell"
-    }
+  backend "gcs" {
+    # Managed by gitops
+    bucket = "ai-playground-c437-terraform-state"
+    prefix = "xhaskell"
+  }
 }
