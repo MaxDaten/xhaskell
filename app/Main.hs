d77df45db0f728b8967cb2c02cb9623f1b836274
Jan-Philip Loos <maxdaten@gmail.com>
2023-10-24 00:58:00 +0200

    chore: list static dir on startup

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ module Main where
 import Component.Prompt (PromptAPI, promptHandler)
 import Component.Root (root)
 import Data.Functor.Identity (Identity)
+import GHC.OldList (sort)
 import Lucid
 import Network.Wai.Handler.Warp (run)
 import Servant
@@ -24,6 +25,7 @@ import Servant
   )
 import Servant.HTML.Lucid (HTML)
 import ServerSettings (ServerSettings (..), serverSettingsWithEnv)
+import System.Directory (listDirectory)
 import Text.Printf (printf)
 
 type API =
@@ -41,6 +43,8 @@ main = mainWithSettings =<< serverSettingsWithEnv
 mainWithSettings :: ServerSettings -> IO ()
 mainWithSettings settings@ServerSettings {..} = do
   printf "%s\n" (show settings)
+  listDirectory staticDir >>= mapM_ (printf "%s\n") . sort
+
   printf "Visit: http://localhost:%d/index.html\n" port
   run port (app settings)
 
